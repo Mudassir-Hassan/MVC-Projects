39efcbc41eeb4fb16c653619b4cdba298520db64
Muhammad Mudassir Hassan <mudassirhassan.27@hotmail.com>
2016-08-12 14:07:37 +0500

    Add files via upload

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,140 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+    public class BooksController : Controller
+    {
+        private Booksdbcontext db = new Booksdbcontext();
+
+        ////Search String
+       public ActionResult Index (string searchstring)
+        {
+            var Books = from b in db.Bookss
+                        select b;
+           if (!String.IsNullOrEmpty(searchstring))
+           {
+               Books = Books.Where(s => s.Title.Contains(searchstring));
+           }
+           return View("Index", Books);
+        }
+
+        // GET: /Books/
+        //public ActionResult Index()
+        //{
+        //    return View(db.Bookss.ToList());
+        //}
+
+        // GET: /Books/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Books books = db.Bookss.Find(id);
+            if (books == null)
+            {
+                return HttpNotFound();
+            }
+            return View(books);
+        }
+
+        // GET: /Books/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Books/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,Title,ReleaseDate,Genre,Price,qty")] Books books)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bookss.Add(books);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(books);
+        }
+
+        // GET: /Books/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Books books = db.Bookss.Find(id);
+            if (books == null)
+            {
+                return HttpNotFound();
+            }
+            return View(books);
+        }
+
+        // POST: /Books/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,Title,ReleaseDate,Genre,Price,qty")] Books books)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(books).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(books);
+        }
+
+        // GET: /Books/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Books books = db.Bookss.Find(id);
+            if (books == null)
+            {
+                return HttpNotFound();
+            }
+            return View(books);
+        }
+
+        // POST: /Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Books books = db.Bookss.Find(id);
+            db.Bookss.Remove(books);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/Books.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public class Books
+    {
+        public int ID { get; set; }
+        
+        [StringLength(60,MinimumLength=3)]
+        public string Title { get; set; }
+        
+        [Display(Name="Releasedate")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString="{0:yyyy-MM-dd}", ApplyFormatInEditMode=true) ]
+        public DateTime ReleaseDate { get; set; }
+        
+        [StringLength(30)]
+        [Required]
+        public string Genre { get; set; }
+
+        [Range(1,50)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+
+        private int Qty { get; set; }
+
+        public int qty
+        {
+            get
+            {
+                return Qty;
+            }
+            set
+            {
+                Qty = value;
+            }
+        }
+    }
+   public class Booksdbcontext : DbContext
+   {
+       public DbSet<Books> Bookss { get; set; }
+   }
+}
